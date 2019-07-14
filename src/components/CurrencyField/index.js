9a82e80ed9e5158cde07900a6ffe7c7a43a25683
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2019-07-13 22:31:52 -0500

    patch: finalize initial UI

--- src/components/CurrencyField/index.js
+++ src/components/CurrencyField/index.js
@@ -5,7 +5,12 @@ function CurrencyField(props) {
   return (
     <fieldset>
       <label htmlFor={props.id}>
-        USD
+        <select>
+          <option>USD</option>
+          <option>MXN</option>
+          <option>EUR</option>
+          <option>CAN</option>
+        </select>
       </label>
       <input id={props.id} type="number" className="currency_field"/>
     </fieldset>
