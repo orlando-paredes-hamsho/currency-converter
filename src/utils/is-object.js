1bab0f268a34c534d018f63a0ea89cd0539b8c82
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2019-07-14 01:05:04 -0500

    patch: add is-object util

---
+++ src/utils/is-object.js
@@ -0,0 +1,4 @@
+const isObject = (object) => (
+  !!(typeof object === 'object' && object !== null && !Array.isArray(object))
+);
+export default isObject;
---
+++ src/utils/is-object.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+import { expect } from "chai";
+import isObject from "./is-object";
+
+const nonObjectList = [[], "string", 1000, null, undefined];
+
+describe("isObject util method", () => {
+  let dataType;
+  nonObjectList.forEach(nonObject => {
+    dataType = !nonObject ? `${nonObject}` : nonObject.constructor.name;
+    test(`${dataType} is not an Object so the method will return false`, () => {
+      expect(isObject(nonObject)).to.be.false;
+    });
+  });
+  test("Object data passed into the method should return a value of true", () => {
+    const anObject = {};
+    expect(isObject(anObject)).to.be.true;
+  });
+});
