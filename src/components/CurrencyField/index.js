01ed76fa0208fb380c1371a95a81f25e42fa7086
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2019-07-15 12:57:42 -0500

    patch: add deferred execution of update currencies on text input on change

--- src/components/CurrencyField/index.js
+++ src/components/CurrencyField/index.js
@@ -5,8 +5,22 @@ import './index.css';
 
 const CurrencyField = observer(
   class CurrencyField extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          timeout: null
+      };
+    }
+    delayUpdateCurrencies = () => {
+        clearTimeout(this.state.timeout);
+        this.setState({
+          timeout: this.generateUpdateTimeout()
+        });
+    };
+    generateUpdateTimeout = () => setTimeout(this.props.update_currencies, 300);
     handleChange = (e) => {
       if (this.props.callback) this.props.callback(e.currentTarget.value);
+      if (this.props.update_currencies) this.delayUpdateCurrencies();
     }
     render() {
       return (
@@ -25,6 +39,7 @@ const CurrencyField = observer(
 CurrencyField.propTypes = {
   id: PropTypes.string,
   callback: PropTypes.func,
+  update_currencies: PropTypes.func,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
--- src/components/CurrencyField/index.test.js
+++ src/components/CurrencyField/index.test.js
@@ -5,7 +5,12 @@ import CurrencyField from './index';
 import mock_currencies from '../../test-data/formatted_currencies';
 const id = 'test'
 
+jest.useFakeTimers();
+
 describe('CurrencyField', () => {
+  beforeEach(() => {
+    jest.clearAllTimers();
+  });
   describe('Without Props', () => {
     it('Renders', () => {
         const wrapper = shallow(<CurrencyField />);
@@ -14,12 +19,78 @@ describe('CurrencyField', () => {
   });
   describe('With Props', () => {
     it('Renders with props', () => {
-        const wrapper = shallow(<CurrencyField id={id} currencies={mock_currencies}/>);
+        const wrapper = shallow(<CurrencyField id={id} />);
         expect(wrapper.getElements()).not.to.deep.equal([null]);
     });
     it('Contains an input with the provided id', () => {
-        const wrapper = shallow(<CurrencyField id={id} currencies={mock_currencies} />);
+        const wrapper = shallow(<CurrencyField id={id} />);
         expect(wrapper.find(`input#${id}`)).to.exist;
+    });
+  });
+  describe('delayUpdateCurrencies', () => {
+    it('Calls clearTimeout', () => {
+        const update_currencies = jest.fn();
+        const wrapper = mount(
+          <CurrencyField
+            update_currencies={update_currencies}
+          />
+        );
+        wrapper.instance().delayUpdateCurrencies();
+        expect(clearTimeout.mock.calls.length).to.equal(1);
+    });
+    it('Calls generateUpdateTimeout', () => {
+        const update_currencies = jest.fn();
+        const wrapper = mount(
+          <CurrencyField
+            update_currencies={update_currencies}
+          />
+        );
+        wrapper.instance().generateUpdateTimeout = jest.fn()
+        wrapper.instance().delayUpdateCurrencies();
+        expect(wrapper.instance().generateUpdateTimeout.mock.calls.length).to.equal(1);
+    });
+  });
+  describe('handleChange', () => {
+    it('Calls the callback on change', () => {
+        const callback = jest.fn();
+        const wrapper = mount(
+          <CurrencyField
+            callback={callback}
+          />
+        );
+        wrapper.instance().handleChange({ currentTarget: { value: 1 } });
+        expect(callback.mock.calls.length).to.equal(1);
+    });
+    it('Calls delayUpdateCurrencies on change', () => {
+        const update_currencies = jest.fn();
+        const wrapper = mount(
+          <CurrencyField
+            update_currencies={update_currencies}
+          />
+        );
+        wrapper.instance().delayUpdateCurrencies = jest.fn()
+        wrapper.instance().handleChange({ currentTarget: { value: 1 } });
+        expect(wrapper.instance().delayUpdateCurrencies.mock.calls.length).to.equal(1);
+    });
+    it('Does not call delayUpdateCurrencies on change without update_currencies', () => {
+        const update_currencies = jest.fn();
+        const wrapper = mount(
+          <CurrencyField />
+        );
+        wrapper.instance().delayUpdateCurrencies = jest.fn()
+        wrapper.instance().handleChange({ currentTarget: { value: 1 } });
+        expect(wrapper.instance().delayUpdateCurrencies.mock.calls.length).to.equal(0);
+    });
+    it('Calls update_currencies on change after the timer ends', () => {
+        const update_currencies = jest.fn();
+        const wrapper = mount(
+          <CurrencyField
+            update_currencies={update_currencies}
+          />
+        );
+        wrapper.instance().handleChange({ currentTarget: { value: 1 } });
+        jest.runAllTimers();
+        expect(update_currencies.mock.calls.length).to.equal(1);
     });
   });
 });
